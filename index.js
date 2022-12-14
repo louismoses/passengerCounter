51f120a691b6aef9d478d2ccb57a826ec2b7462d
louis moses <louismoses.rait@gmail.com>
2022-12-14 21:32:24 +0800

    passenger per bus

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-// document.getElementById("count-el").innerText = 5
+let redBusCount = 0;
+let blueBusCount = 0;
+let greenBusCount = 0;
+let countElRed = document.getElementById("count-el-red");
+let countElBlue = document.getElementById("count-el-blue");
+let countElGreen = document.getElementById("count-el-green");
 
-let count = 1
-
-count += 1
-
-
-console.log(count)
+function incrementRed() {
+  redBusCount += 1;
+  console.log(redBusCount);
+  countElRed.innerText = redBusCount;
+}
+function incrementBlue() {
+  blueBusCount += 1;
+  console.log(blueBusCount);
+  countElBlue.innerText = blueBusCount;
+}
+function incrementGreen() {
+  greenBusCount += 1;
+  console.log(greenBusCount);
+  countElGreen.innerText = greenBusCount;
+}
