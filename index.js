d50323e675f02b64e8c0f8e84b0bd6a2ea59537b
louis moses <louismoses.rait@gmail.com>
2022-12-14 22:17:18 +0800

    added a save button

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ let countElGreen = document.getElementById("count-el-green");
 
 function incrementRed() {
   redBusCount += 1;
-  console.log(redBusCount);
   countElRed.innerText = redBusCount;
 }
 function incrementBlue() {
   blueBusCount += 1;
-  console.log(blueBusCount);
   countElBlue.innerText = blueBusCount;
 }
 function incrementGreen() {
   greenBusCount += 1;
-  console.log(greenBusCount);
   countElGreen.innerText = greenBusCount;
+}
+function save() {
+  console.log(redBusCount + greenBusCount + blueBusCount);
 }
